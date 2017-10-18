09ee5d6c4a3351464527a458ce1c3163f5a33204
ArnTheLongBeard <arn.menant@gmail.com>
2017-10-18 15:30:39 +0200

    Continue the tutorial with Clock

--- clock.elm
+++ clock.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html)
+import Html exposing (Html, button, div)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -16,32 +17,46 @@ main =
 
 -- MODEL
 
-type alias Model = Time
+type alias Model = { time : Time
+   , shouldStop : Bool
+   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+ (Model 0 False, Cmd.none)
 
 
 -- UPDATE
 
 type Msg
   = Tick Time
+  | Pause
+  | Continue
+
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ( {model | time = newTime}, Cmd.none)
+    Pause ->
+       ({model | shouldStop = True }, Cmd.none)
+    Continue ->
+       ({model | shouldStop = False }, Cmd.none)
+
 
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+   if model.shouldStop == True then
+     Sub.none
+   else
+     Time.every second Tick
+
 
 
 -- VIEW
@@ -50,7 +65,7 @@ view : Model -> Html Msg
 view model =
   let
     angle =
-      turns (Time.inMinutes model)
+      turns (Time.inMinutes model.time)
 
     handX =
       toString (50 + 40 * cos angle)
@@ -58,7 +73,17 @@ view model =
     handY =
       toString (50 + 40 * sin angle)
   in
-    svg [ viewBox "0 0 100 100", width "300px" ]
+   div []
+    [ svg [ viewBox "0 0 100 100", width "300px" ]
       [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
       , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
       ]
+
+    , clockButton model ]
+
+clockButton : Model -> Html Msg
+clockButton model =
+    if model.shouldStop == True then
+        button [ onClick Continue ] [ text "Continue"]
+    else
+        button [ onClick Pause ] [ text "Pause"]
